d44bbdb1f83b3d552058a77064ce82f4e936a7ee
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2022-03-27 00:29:40 +0300

    Set Up Starboard

---
+++ HomeWork-23/HomeWork-23/Controller/TwoVC.swift
@@ -0,0 +1,37 @@
+//
+//  TwoVC.swift
+//  HomeWork-23
+//
+//  Created by Ваня Науменко on 26.03.22.
+//
+
+import UIKit
+
+class TwoVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var stakView: UIStackView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func buttenOne(_ sender: Any) {
+    }
+    @IBAction func buttenTwo(_ sender: Any) {
+    }
+}
